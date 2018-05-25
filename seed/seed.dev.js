f938904f590547471db1d1b664cd2e23a2f348a5
Seth Bell <seth@Seths-MacBook-Pro.local>
2018-05-25 15:27:53 +0100

    slight tweaks to console messages

--- seed/seed.dev.js
+++ seed/seed.dev.js
@@ -7,12 +7,11 @@ const { DB_URL }  = require('../config');
 mongoose.connect(DB_URL)
   .then(() => seedDB())
   .then((docs) => {
-    console.log(DB_URL)
-    console.log(`${process.env.NODE_ENV} DB sucessfully seeded with ${docs[0].length} comments, ${docs[1].length} articles, ${docs[2].length} users, ${docs[3].length} topics`)
+    console.log(`DB "${DB_URL}" sucessfully seeded with ${docs[0].length} comments, ${docs[1].length} articles, ${docs[2].length} users, ${docs[3].length} topics`)
     return mongoose.disconnect();
   })
   .then(() => {
-    console.log(`Sucessfully disconnected from ${process.env.NODE_ENV} DB`)
+    console.log(`Sucessfully disconnected from DB`)
   })
   .catch((err) => {
     console.log(err);
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -15,15 +15,13 @@ describe('/api', () => {
   beforeEach(() => {
     return seedDB()
     .then(docs => {
-      // console.log(`${process.env.NODE_ENV} DB sucessfully seeded with ${docs[0].length} comments, ${docs[1].length} articles, ${docs[2].length} users, ${docs[3].length} topics`);
-
       [comments, articles, users, topics] = docs
     })
   });
 
   after(() => {
     return mongoose.disconnect()
-    .then(() => console.log(`Sucessfully disconnected from ${process.env.NODE_ENV} DB`))
+    .then(() => console.log(`Sucessfully disconnected from DB`))
   })
 
   describe('/', () => {
